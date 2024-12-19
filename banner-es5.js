ee25e1f3886e58d276e958f72ca5b0297358d0b2
Andrew Zamler-Carhart <andrewzc@synamedia.com>
2024-12-19 17:35:51 +0100

    Updated to use senza.ShakaPlayer.

--- banner-es5.js
+++ banner-es5.js
@@ -1,26 +1,49 @@
 const TEST_VIDEO = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
 
-window.addEventListener("load", function() {
+let player;
+
+window.addEventListener("load", async () => {
   try {
-    senza.init().then(function() {
-      initVideo(new shaka.Player(document.getElementById("video")));
-      loadVideo(TEST_VIDEO).then(function () {
-        playVideo();
-        senza.uiReady();
-      });
-    });
+    await senza.init();
+    player = new senza.ShakaPlayer(video);
+    await player.load(TEST_VIDEO);
+    await video.play();
+    senza.uiReady();
   } catch (error) {
     console.error(error);
   }
 });
 
-document.addEventListener("keydown", async function(event) {
-	switch (event.key) {
-    case "Enter": toggleBackground(); break;
-    case "Escape": playPause(); break;
+document.addEventListener("keydown", async function (event) {
+  switch (event.key) {
+    case "Enter": await toggleBackground(); break;
+    case "Escape": await playPause(); break;
     case "ArrowLeft": skip(-30); break;
-    case "ArrowRight": skip(30); break;      
-		default: return;
-	}
-	event.preventDefault();
+    case "ArrowRight": skip(30); break;
+    default: return;
+  }
+  event.preventDefault();
 });
+
+async function toggleBackground() {
+  if (senza.lifecycle.state == senza.lifecycle.UiState.BACKGROUND) {
+    await senza.lifecycle.moveToForeground();
+  } else {
+    // This is currently required to sync the players before switching.
+    // It will no longer be necessary in an upcoming version of the SDK.
+    player.remotePlayer.currentTime = video.currentTime;
+    await senza.lifecycle.moveToBackground();
+  }
+}
+
+async function playPause() {
+  if (video.paused) {
+    await video.play();
+  } else {
+    await video.pause();
+  }
+}
+
+function skip(seconds) {
+  video.currentTime = video.currentTime + seconds;
+}
